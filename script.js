d9fc750d56a29b98d58a2bf5d0ef96e2cede2db7
lachgar <lachgar.m@gmail.com>
2018-12-05 22:39:40 +0100

    modif

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ var id = 0;
 function load() {
     if (typeof localStorage != 'undefined') {
         if ('e' in localStorage) {
-            var contenu = $('#contenu');
-            var ligne = '';
-            var indice = 0;
-            tab = JSON.parse(localStorage.getItem('e'));
-            tab.forEach(element => {
-                ligne += '<tr><td>' + element.nom + '</td><td>' + element.prenom + '</td><td>' + element.email + '</td><td><a class="delete" href="" indice="' + indice + '">Supprimer</a></td><td><a indice="' + indice + '" class="update" href="">Modifier</a></td></tr>';
-                indice++;
-            });
-            contenu.html(ligne);
+            //traitement
         }
     } else {
         alert("localStorage n'est pas supporté");
@@ -21,42 +13,24 @@ function load() {
 $(document).ready(function () {
     load();
     $('#valider').click(function () {
-        var nom = $('#nom').val();
-        var prenom = $('#prenom').val();
-        var email = $('#email').val();
-        var e = {
-            'nom': nom,
-            'prenom': prenom,
-            'email': email
-        };
-        if ($('#valider').html() == 'Modifier') {
-            tab[id] = e;
-            $('#valider').html("Valider");
-        } else {
-            tab.push(e);
-        }
-        localStorage.setItem('e', JSON.stringify(tab));
-        load();
+        //Traitement
     });
 
     $('#contenu').on('click', '.delete', function () {
         //var id = parseInt( $(this).parent().index() )+1; //colonne index
         id = parseInt($(this).attr('indice'));
         alert(id);
-        tab.splice(id, 1);
-        localStorage.clear;
-        localStorage.setItem('e', JSON.stringify(tab));
-        load();
+        tab.splice(id, 1); //permet de supprimer un objet dans la position id 
+        //Traitement
     });
 
     $('#contenu').on('click', '.update', function () {
         //var id = parseInt( $(this).parent().index() )+1; //colonne index
         id = parseInt($(this).attr('indice'));
-        $('#nom').val(tab[id].nom);
-        $('#prenom').val(tab[id].prenom);
-        $('#email').val(tab[id].email);
-        $('#valider').html('Modifier');
-        return false;
+        //Traitement
     });
 });
+
+
+
 
